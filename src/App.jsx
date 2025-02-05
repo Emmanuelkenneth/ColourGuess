5176b8a4915cd1b6c84242b096d596773b3af0f3
emmanuel kenneth <emmanuelkenneth49@gmail.com>
2025-02-05 11:55:00 -0800

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,92 @@
+import { useState, useEffect } from "react";
+import "./App.css";
+
+function App() {
+  const [targetColor, setTargetColor] = useState("");
+  const [isGuessCorrect, setIsGuessCorrect] = useState("Make a guess");
+
+  const [options, setOptions] = useState([
+    "#3F7EB5",
+    "#D92C5A",
+    "#F4A623",
+    "#7CD38A",
+    "#8E44AD",
+    "#2C3E50",
+  ]);
+  const [message, setMessage] = useState("");
+  const [score, setScore] = useState(0);
+  const randomColour = Math.floor(Math.random() * options.length);
+  const hexValue = options[randomColour];
+  const guess = isGuessCorrect ? (
+    <h2 className="won"> You Win</h2>
+  ) : (
+    <h2 className="lost">Try Again</h2>
+  );
+  useEffect(
+    function randomColor() {
+      setTargetColor(hexValue);
+    },
+    [targetColor]
+  );
+  function runCheck(guess) {
+    const newTargetColor = Math.floor(Math.random() * options.length);
+    const targetColorValue = options[newTargetColor];
+    if (guess === hexValue) {
+      setMessage("You win");
+      setScore(score + 1);
+      setTargetColor(targetColorValue);
+      setIsGuessCorrect(true);
+    } else if (guess !== hexValue) {
+      setMessage("Try Again");
+      setTargetColor(targetColorValue);
+      setIsGuessCorrect(false);
+    }
+  }
+  function reset() {
+    const newTargetColor = Math.floor(Math.random() * options.length);
+    const targetColorValue = options[newTargetColor];
+    setScore(0);
+    setMessage("");
+    setTargetColor(targetColorValue);
+  }
+
+  return (
+    <>
+      <div className="container">
+        <h1>Guess the correct colour</h1>
+        {score === 0 ? <h2>Make a guess</h2> : guess}
+
+        <h3>Score: {score}</h3>
+        <button className="resetBtn" onClick={reset}>
+          New Game
+        </button>
+        <div
+          style={{
+            backgroundColor: targetColor,
+          }}
+          className="target-color"
+          data-test-id="colorBox"
+        >
+          {targetColor}
+        </div>
+
+        <ul className="guesses" data-test-id="colorOptions">
+          {options.map((option) => (
+            <li>
+              <button
+                onClick={() => runCheck(option)}
+                style={{
+                  backgroundColor: option,
+                  height: "80px",
+                  width: "100px",
+                }}
+              ></button>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </>
+  );
+}
+
+export default App;
